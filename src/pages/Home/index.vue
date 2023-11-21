5089d472e42a767b21cab77fabda4481ae1a64c9
cora <cora33143@gmail.com>
2023-11-21 10:58:17 +0800

    feat: add font and mixin with text, rwd size

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,6 +1,27 @@
 <script setup></script>
 
 <template>
-  <div>home</div>
+  <div class="text1">text1</div>
+  <div class="text2">text2</div>
+  <div class="text3">text3</div>
+  <div class="text4">text4</div>
+  <div class="text5">text5</div>
 </template>
-<style lang="scss" scoped></style>
+
+<style lang="scss" scoped>
+.text1 {
+  @include text1;
+}
+.text2 {
+  @include text2;
+}
+.text3 {
+  @include text3;
+}
+.text4 {
+  @include text4;
+}
+.text5 {
+  @include text5;
+}
+</style>
